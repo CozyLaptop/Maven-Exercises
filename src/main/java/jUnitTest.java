a10133d596ddd5c6e0597f68b0aaf53072ee0761
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-29 10:58:12 -0500

    finished first half of mini exercise

---
+++ src/main/java/jUnitTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class jUnitTest {
+//    @Test
+//    public void testTwoIsTwo(){
+//        assertEquals(3,2);
+//    }
+    @Test
+    public void testAssertions(){
+        int num = 2;
+        assertEquals(2, num);
+    }
+
+}
---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+    @Test
+    public void isCodeup(){
+        System.out.println("Checking Codeup");
+        assertEquals("Codeup", "Codeup");
+    }
+    @Test
+    public void isLanguages(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+        System.out.println("Checking languages");
+        assertNotSame(languages, moreLanguages);
+    }
+    @Test
+    public void isNumbersArray(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+        assertArrayEquals(numbers, otherNumbers);
+    }
+    @Test
+    public void containsLetter(){
+//        Scanner sc = new Scanner(System.in);
+//        String language = sc.nextLine();
+        String language = "PHP";
+        System.out.println(language.contains("H"));
+        System.out.println(language.contains("J"));
+        assertFalse(language.contains("J"));
+        assertTrue(language.contains("H"));
+    }
+}
